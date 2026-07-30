8595f7d380ee76ec8078c56aea996b64189afb0b
jai38 <malanijai05@gmail.com>
2026-07-31 01:57:26 +0530

    feat(ci): update AWS S3 + CloudFront deploy pipeline and vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/wellmindly-frontend-university/' : '/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
